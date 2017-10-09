4a223fce4c1b17eaa54d90909787ccb133e153a3
Justine <31459557+justinegiardina@users.noreply.github.com>
2017-10-09 15:24:32 -0400

    Week5 homework

--- week4homework/sketch.js
+++ week4homework/sketch.js
@@ -1,13 +1,20 @@
 var circX;
 var circY;
+var circleX = 0;
+var speed = 10;
 var circDiameter = 350;
 var circRadius = circDiameter / 2;
 var d;
 var redCol = 200;
 var img;
-
-
-var isClicked = false;
+var buttonClicked = false;
+var rectX = 100;
+var rectY = 100;
+var rectSize = 100;
+var rectTop = rectY;
+var rectBottom = rectY + rectSize;
+var rectLeft = rectX;
+var rectRight = rectX + rectSize;
 
 function preload() {
     img = loadImage("bun.png");
@@ -21,6 +28,7 @@ function setup() {
     circY = windowHeight / 2;
       textFont(swissliFont);
     textSize(30);
+    
 }
 
 function draw() {
@@ -29,6 +37,7 @@ function draw() {
     noStroke();
     ellipse(circX, circY, circDiameter, circDiameter);
     d = dist(circX, circY, mouseX, mouseY);
+    
     
 
     if (d < circRadius) {
@@ -39,18 +48,24 @@ function draw() {
         redCol = 200;
            imageMode(CENTER);
     image(img, width/2, height/2, 200, 200);
-            text("There is a bunny that's awesome", mouseX, mouseY, 200, 800);
+            text("There is a bunny AND a bouncing object that's awesome", mouseX, mouseY, 200, 800);
+            ellipse(circleX, 300, 100, 100);
+    if(circleX > width || circleX < 0){
+        speed = speed *-1;
     }
-    
-}
+    circleX = circleX + speed;
+        
+    }
 
-function mousePressed() {
-    if (d < circRadius) {
-        redCol = 0;
-    } else {
+function mousePressed(){
+    if(d < circRadius && buttonClicked==true){
         redCol = 200;
+           imageMode(CENTER);
+    image(img, width/2, height/2, 200, 200);
+            text("Oh yay he is back!", mouseX, mouseY, 200, 800);
+    }else{
+        redCol = 0;
     }
-    
-
+    }
 
 }
