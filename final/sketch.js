69545968dabb1a2bb06e51503a88469b5e6c1c68
Justine <31459557+justinegiardina@users.noreply.github.com>
2017-12-05 19:23:40 -0500

    Add files via upload

--- final/sketch.js
+++ final/sketch.js
@@ -13,7 +13,7 @@ var img2;
 var img3;
 var isClicked = false;
 var audio;
-var index = 0;
+
 var increment=280;
 var xpos1 =-80;
 var xpos2 = 60;
@@ -25,11 +25,17 @@ var rectBottom = rectY + rectSize;
 var rectLeft = rectX;
 var rectRight = rectX + rectSize;
 var buttonClicked = false;
+var randomPull;
+var Pull;
+var fortuneRendered = false;
+var fortuneTrue = false;
 
-var sun = ["A cow fed roses is still a cow.", "Punishment is longer remembered than reward.", "Anger is useful if channeled correctly.", "Being alone is productive, being isolated is detrimental.", "Honesty is uncomfortable.", "Adamant attachments stem from early inconsistencies.", "Kill your darlings, fast.", "Ambivalence will prevail.", "Considering posibilities isn't always useful.", "Remember to forget."];
 
-var moon = ["Dreams are a medium for internal communications.", "1-Universal connection is objectively good.", "The most meaningful phenomena cannot be communicated.", "Passion is important but rare.", "Purity is always boring.", "Realism is difficult but necessary.", "Denial is sometimes good.", "Excess is over-dramatized.", "If it's important you can't afford to be shy about it.", "Suffering isn't worth it."];
+var sun = ["A cow fed roses is still a cow.", "Punishment is longer remembered than reward.", "Anger is useful if channeled correctly.", "Being alone is productive, being isolated is detrimental.", "Honesty is uncomfortable.", "Adamant attachments stem from early inconsistencies.", "Kill your darlings, fast.", "Ambivalence will prevail.", "Considering posibilities isn't always useful.", "Remembering is sometimes unnecessary martyrdom."];
 
+var moon = ["Dreams are a medium for internal communications.", "Universal connection is objectively good.", "The most meaningful phenomena cannot be communicated.", "Passion is important but rare.", "Purity is always boring.", "Realism is difficult but necessary.", "Denial is sometimes good.", "Excess is over-dramatized.", "If it's important you can't afford to be shy.", "Suffering isn't worth it."];
+
+var index = 9;
 
 function preload(){
             regina = loadFont("swissli.ttf");
@@ -49,6 +55,7 @@ function setup(){
     circY = 400;
     circlX = 600;
     circlY = 100;
+
 }
 
 function draw(){
@@ -56,31 +63,11 @@ function draw(){
     background(img);
     background(random(240), random (30), random(200), random(75));
     
-    fill(255,255,255,75)
-    rect(400,400,400,400);
-    fill(0,255,0);
-        ellipse(200,200,200,200);
-    
     
     for (var i=0; i<12;i++){
         draweyes(xpos2 + increment*i, random(300), 100);
         draweyes(xpos1 + increment*i, random(200), 100);
     }
-//        draweyes(1180, random(300), 100);
-//        draweyes(1040, random(200), 100);
-//        
-//        draweyes(900, random(300), 100);
-//        draweyes(760, random(200), 100);
-//        
-//        draweyes(xpos2 + increment *2, random(300), 100);
-//        draweyes(xpos1 + increment *2, random(200), 100);
-//        
-//        draweyes(xpos2 + increment*1, random(300), 100);
-//        draweyes(xpos1 + increment*1, random(200), 100);
-//        
-//        draweyes(xpos2 + increment*0, random(300), 100);
-//        draweyes(xpos1 + increment*0, random(200), 100);
-//    
     
     fill (255, 255, 255, 0);
     noStroke();
@@ -95,19 +82,30 @@ function draw(){
         fill (5, 0, 0);
         text("Enlighten.", mouseX, mouseY, 200, 800);
             background(240, 60, 100, 75);
+            fill(255);
+            
+        if(fortuneRendered){
+            text(sun[randomPull], width/4, height/2);
+        }
+
+            
     } else if (d2 < circlRadius) {
             image(img3, 600, 100, 300, 300);
         fill (0, 0, 5);
         text("Illuminate.", mouseX, mouseY, 200, 800);
-            background(70, 60, 240, 70);} else {    
+        background(70, 60, 240, 70);
+        fill(255);
+
+        if(fortuneTrue){
+            text(moon[randomPull], width/4, height/2);
+        }
+
+            } else {    
     image(img2, 400, 400, 250, 250);
                     image(img3, 600, 100, 335, 335);
        text("Click one to have your fortune told.", mouseX, mouseY, 400, 800);    
-    }    
-    
-    
+    }
 }
-
 
 function draweyes(x,y,onresize){    
 fill (204, random(51), 0, 100);
@@ -117,35 +115,13 @@ fill (204, random(51), 0, 100);
 }
 
 function mousePressed(){
-    if  (d < circRadius){
-        fill(255,0,0);
-        ellipse(100,100,800,800);
-       text(random(sun[index]), 50, height/2);
-        }
-    else{
-        fill(255);
-        ellipse(200,200,800,800);
-        text(random(sun[index]), 50, height/2);
-    }
-    
-    if (d2 < circlRadius) {
-              fill(255);
-        ellipse(200,200,800,800);
-        text(random(moon[index]), 50, height/2);
-        }   else{
-            text(random(moon[index]), 50, height/2);
-        }
-    if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && buttonClicked==false){
-        fill(255);
-        ellipse(200,200,200,200);
-        buttonClicked = !buttonClicked;
-    }else if (mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && buttonClicked==true){
-        fill(255,0,0);
-        ellipse(200,200,200,200);
-        buttonClicked = !buttonClicked;
+    if (d < circRadius) {
+        randomPull = floor(random(0,sun.length));
+        fortuneRendered = !fortuneRendered;
+        
+    } if (d < circlRadius) {
+        randomPull = floor(random(0,moon.length));
+        fortuneTrue = !fortuneTrue;
     }
 }
-
-
-
 
